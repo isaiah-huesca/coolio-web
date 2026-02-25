dc65b20cf2734b3f0494c5df0835d9b498ba0761
isaiah-huesca <ihuesca08@gmail.com>
2026-02-25 17:48:09 +0000

    Wed, Feb 25, 2026, 9:48 AM -08:00

--- script/a.js
+++ script/a.js
@@ -1,7 +1,6 @@
     box = document.querySelector("div")
 
-function size(1) {
-    box.width += Math.random(-1,1)
-}
 
-setInterval(size, 50)
+setInterval(function() {
+  box.width += Math.random(-5,5)
+}, 100);
