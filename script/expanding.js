2041956bbc1ab08eee36f6c1194b71f31430d37b
isaiah-huesca <ihuesca08@gmail.com>
2026-03-02 10:25:52 -0800

    web

---
+++ script/expanding.js
@@ -0,0 +1,15 @@
+panels = document.querySelectorAll('.panel')
+console.log(panels)
+
+
+for (let i = 0; i < 5; i++) {
+    panels[i].addEventListener('click', () => {
+        for (let i = 0; i, 5; i++) {
+                console.log(i)
+
+            panels[i].classList.remove('active')
+        }
+        panels[i].classList.add('active')
+    })
+    console.log(i)
+}
