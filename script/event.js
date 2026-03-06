1da1676f0b660e82bc68d2f2015deeecd553c9d2
isaiah-huesca <ihuesca08@gmail.com>
2026-03-06 17:40:44 +0000

    Fri, Mar 6, 2026, 9:40 AM -08:00

---
+++ script/event.js
@@ -0,0 +1,90 @@
+clbx = document.querySelector(".click")
+spbx = document.querySelector(".spin")
+kbx = document.querySelector(".key")
+hbx = document.querySelector(".hover")
+dblbx = document.querySelector(".dbcl")
+
+
+
+clbx.addEventListener('click', () => {
+
+    cr = Math.random() * 256
+    cg = Math.random() * 256
+    cb = Math.random() * 256
+    console.log("red ")
+    console.log(cr)
+    console.log("green ")
+    console.log(cg)
+    console.log("blue ")
+    console.log(cb)
+    clbx.style.backgroundColor = `rgb(${cr} ${cg} ${cb})`
+})
+
+spbx.addEventListener('click', () => {
+    spbx.classList.toggle('start')
+})
+
+x = 0
+y = 0
+rot = 0
+sec = 0
+document.addEventListener('keydown', (event) => {
+    if (event.key == 'ArrowRight') {
+        x += 10
+        rot += 90
+    }
+    if (event.key == 'ArrowUp') {
+        y -= 10
+        rot -= 90
+
+    }
+    if (event.key == 'ArrowLeft') {
+        x -= 10
+        rot -= 90
+
+    }
+    if (event.key == 'ArrowDown') {
+        y += 10
+        rot += 90
+
+    }
+    sec = Math.floor(Math.random() * 10 + 1)
+    kbx.style.transform = `translate(${x}px,${y}px )`
+    kbx.style.transform = `rotateY(${rot}deg ${sec}s ease)`
+    console.log(rot, sec)
+
+})
+
+hbx.addEventListener('mouseenter', () => {
+    hover = 1
+    console.log(true)
+    setInterval(function() {
+        if (hover == 1) {
+            console.log(true)
+            cr = Math.random() * 256
+            cg = Math.random() * 256
+            cb = Math.random() * 256
+            console.log("red ")
+            console.log(cr)
+            console.log("green ")
+            console.log(cg)
+            console.log("blue ")
+            console.log(cb)
+            hbx.style.backgroundColor = `rgb(${cr} ${cg} ${cb})`
+        }
+    }, 100)
+
+})
+hbx.addEventListener('mouseleave', () => {
+    hover = 0
+    console.log(false)
+})
+
+dblbx.addEventListener('dblclick', () => {
+    setInterval(function() {
+        window.open("why.html")
+    }, 100)
+    setInterval(function() {
+        window.close("event.html")
+    }, 200)
+})
