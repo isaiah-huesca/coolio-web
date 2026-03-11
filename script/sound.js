e9b7c0b94a42f4a4218ddd9ec5c215fe640751e8
isaiah-huesca <ihuesca08@gmail.com>
2026-03-11 16:58:55 +0000

    Wed, Mar 11, 2026, 9:58 AM -07:00

--- script/sound.js
+++ script/sound.js
@@ -2,10 +2,10 @@ const buttons = document.querySelector('.butons')
 const sounds = document.querySelectorAll('audio')
 for(let sound of sounds){
 btn = document.createElement('button')
+btn.classList.add('btn')
+btn.innerText = "click me"
 
 }
-btn.innerText = "click me"
-btn.classList.add('btn')
 buttons.addEventListener('click', () => {
     boo.play()
 })
