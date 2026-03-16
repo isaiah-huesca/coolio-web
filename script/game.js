657de809bae03f2f7ed71e9e0ad5770e8c9f0db3
isaiah-huesca <ihuesca08@gmail.com>
2026-03-16 16:27:11 +0000

    Mon, Mar 16, 2026, 9:27 AM -07:00

--- script/game.js
+++ script/game.js
@@ -4,3 +4,27 @@ const ball
 x = 0
 y = 0
 
+
+const keysPressed = {};
+
+document.addEventListener('keydown', (event) => {
+  // Use event.code to track physical keys consistently
+  if (!event.repeat) { // Ignore repeated keydown events when key is held down
+    keysPressed[event.code] = true;
+  }
+});
+
+document.addEventListener('keyup', (event) => {
+  keysPressed[event.code] = false;
+});
+
+// In a game loop or animation frame:
+function updateGame() {
+  if (keysPressed['ArrowLeft']) {
+    // Move character left
+  }
+  if (keysPressed['ArrowRight']) {
+    // Move character right
+  }
+  // Call this function within window.requestAnimationFrame or a setInterval
+}
