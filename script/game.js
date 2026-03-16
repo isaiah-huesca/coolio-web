dcd0cf11038f3b2a8e19b04bebaf0e886deee9b1
isaiah-huesca <ihuesca08@gmail.com>
2026-03-16 16:35:25 +0000

    Mon, Mar 16, 2026, 9:35 AM -07:00

--- script/game.js
+++ script/game.js
@@ -1,31 +1,34 @@
-const player
-const bot
-const ball
+const player = 0
+const bot = 0
+const ball = 0
 x = 0
 y = 0
 
 
 const keysPressed = {};
 document.addEventListener('keydown', (event) => {
-  // Use event.code to track physical keys consistently
-  if (!event.repeat) { // Ignore repeated keydown events when key is held down
-    keysPressed[event.code] = true;
-  }
+    // Use event.code to track physical keys consistently
+    if (!event.repeat) { // Ignore repeated keydown events when key is held down
+        keysPressed[event.code] = true;
+    }
 });
 
 document.addEventListener('keyup', (event) => {
-  keysPressed[event.code] = false;
+    keysPressed[event.code] = false;
 });
 
 // In a game loop or animation frame:
 function updateGame() {
-  if (keysPressed['ArrowLeft']) {
-    console.log("left")
-  }
-  if (keysPressed['ArrowRight']) {
-    console.log("right")
-  }
-  // Call this function within window.requestAnimationFrame or a setInterval
+    if (keysPressed['ArrowLeft']) {
+        console.log("left")
+    }
+    else if (keysPressed['ArrowRight']) {
+        console.log("right")
+    }
+    else{
+        console.log("N/A")
+    }
+    // Call this function within window.requestAnimationFrame or a setInterval
 }
 
-let intervalID = setInterval(updateGame, 50);
+let intervalID = setInterval(updateGame, 100);
