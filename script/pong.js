4f9f15a3824b426ba55e077a019cf9888b50739d
isaiah-huesca <ihuesca08@gmail.com>
2026-03-25 10:01:27 -0700

    web

--- script/pong.js
+++ script/pong.js
@@ -1,14 +1,13 @@
-
-
 const keysPressed = {};
-i=0
+
+
+
 document.addEventListener('keydown', (event) => {
     // Use event.code to track physical keys consistently
     if (!event.repeat) { // Ignore repeated keydown events when key is held down
+        keysPressed[] = false;
         keysPressed[event.code] = true;
-
     }
-    console.log(keysPressed[i])
-    i++
+console.log(keysPressed)
 });
 
